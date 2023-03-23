7613f54c648c501a91b2d76db7f74fbb0ce212f9
1juanherrera <juansebas751@gmail.com>
2023-03-23 18:22:17 -0500

    intergalactic-app

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+const App = () => {
+
+  return (
+    <div className="galaxy">
+      <div className="galaxy__content">
+        <h1>Comité intergaláctico</h1>
+        <h2>Hola habitante planetario, has sido invitado a hacer parte de esta reunion intergaláctica, para entender esta misión que tiene como objetivo mejorar las condiciones de vida de los habitantes de cada planeta, mi nombre es Juan Herrera, en presentación de la orden intergaláctica y te voy a enseñar todo lo que necesitas saber.</h2>
+      </div>
+      <div className="galaxy__presentation">
+      <div className="galaxy__presentation-container-1">
+        <p>Lo primero que necesitas saber es ¿Qué es el internet? para dar el paso esta nueva era que vendrá.</p><br />
+        <p>¿Qué es el internet?</p><br />
+        <p>El internet es una red global de computadoras interconectadas que permite la comunicación y transferencia de información a través de diferentes dispositivos electrónicos, como computadoras, teléfonos inteligentes, tablets, entre otros.</p><br />
+        <p>En términos simples, el internet es una red de redes que permite a los terrícolas conectarse entre sí y compartir información de manera global y en tiempo real. A través del internet, podemos acceder a una gran cantidad de recursos y servicios, como correo electrónico, redes sociales, sitios web, aplicaciones en línea, juegos en línea, compras en línea, entre otros.</p><br />
+        <p>En los últimos años, el internet se ha convertido en una herramienta fundamental para la vida cotidiana de los terrícolas y ha transformado la forma en que interactuamos, trabajamos y nos comunicamos. Sin embargo, no todos los habitantes de nuestro universo tienen acceso a esta red global de computadoras interconectadas. Por esta razón, el objetivo de este comité intergaláctico es encontrar soluciones para mejorar la conectividad y el acceso a la información en los planetas menos desarrollados.</p>
+      </div>
+      <div className="galaxy__presentation-container-2">
+        <p>Pero para que te puede servir el internet</p><br />
+        <p>¿Para qué sirve el internet?</p><br />
+        <p>Sirve para una amplia variedad de propósitos y beneficios, algunos de los cuales incluyen:</p><br />
+        <ol>
+            <li>Comunicación: El internet permite la comunicación instantánea entre habitante y grupos de habitantes en todo el mundo a través de correo electrónico, chat, redes sociales, videoconferencias y otras herramientas en línea.</li>
+            <li>Acceso a la información: El internet es una fuente inagotable de información, con acceso a noticias, recursos educativos, documentos técnicos, investigaciones y mucho más.</li>
+            <li>Comercio electrónico: El internet facilita el comercio en línea, permitiendo a las empresas y los consumidores comprar y vender bienes y servicios a través de sitios web y aplicaciones de comercio electrónico.</li>
+            <li>Entretenimiento: El internet ofrece una amplia variedad de entretenimiento, como música, películas, videos, juegos, redes sociales y contenido en línea.</li>
+            <li>Trabajo remoto: El internet permite el trabajo remoto, lo que significa que los terrícolas pueden trabajar desde cualquier lugar del mundo y colaborar en tiempo real con colegas y clientes en línea.</li>
+            <li>Investigación y educación: El internet es una herramienta poderosa para la investigación y la educación, permitiendo a los estudiantes y académicos acceder a una gran cantidad de recursos educativos y compartir información con colegas en todo el mundo.</li>
+        </ol>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
